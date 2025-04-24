a931f0db5826843e310b81b3eec0f1c7a063ecf0
Dom Nguyễn <57473628+DAITHANG23@users.noreply.github.com>
2025-04-24 16:34:46 +0700

    feat: update

--- utils/emails.ts
+++ utils/emails.ts
@@ -3,6 +3,7 @@ import SMTPTransport = require("nodemailer/lib/smtp-transport");
 import pug from "pug";
 import { IUserEmail } from "../@types";
 import htmlToText from "html-to-text";
+import path = require("path");
 
 // import { fileURLToPath } from 'url';
 // import path from 'path';
@@ -44,12 +45,17 @@ const Email = class Email {
   // Send the actual email
   async send(template: string, subject: string): Promise<void> {
     // 1) Render HTML based on a pug template
-    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
-      firstName: this.firstName,
-      url: this.url,
-      otp: this.otp,
-      subject,
-    });
+    const html = pug.renderFile(
+      path.join(__dirname, "../views/email", `${template}.pug`),
+      {
+        firstName: this.firstName,
+        url: this.url,
+        otp: this.otp,
+        subject,
+      }
+    );
+
+    console.log("html:", html);
 
     // 2) Define email options
     const mailOptions = {
