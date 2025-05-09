e1cd3cf4ea16f919f692f47844de6803c4bd14b0
Dom Nguyễn <57473628+DAITHANG23@users.noreply.github.com>
2025-05-09 22:07:18 +0700

    feat: Build concept data

--- models/conceptModel.ts
+++ models/conceptModel.ts
@@ -1,52 +1,64 @@
 import mongoose, { Schema } from "mongoose";
 import { IConcept } from "../@types";
 
-const conceptSchema = new mongoose.Schema<IConcept>({
-  name: { type: String, require: [true, "Please provider name."] },
-  description: {
-    type: String,
-    require: [true, "Please provider description."],
-  },
-  address: { type: String, require: [true, "Please provider address."] },
-  images: [{ type: String }],
-  conceptManager: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: [true, "Concept must have a manager"],
-  },
-  totalProfit: {
-    type: Number,
-    require: [true, "Concept Restaurant must be have total profit."],
-  },
-  imageCover: {
-    type: String,
-    required: [true, "Restaurant must be have imageCover."],
-  },
-  timeSlot: [
-    {
-      startTime: String,
-      endTime: String,
-      available: Boolean,
+const conceptSchema = new mongoose.Schema<IConcept>(
+  {
+    name: { type: String, require: [true, "Please provider name."] },
+    description: {
+      type: String,
+      require: [true, "Please provider description."],
     },
-  ],
-  dishes: [
-    {
-      name: { type: String, required: true },
-      description: { type: String },
-      type: { type: String, required: true },
-      image: { type: String },
-      price: { type: Number, required: true },
+    address: { type: String, require: [true, "Please provider address."] },
+    images: [{ type: String }],
+    conceptManager: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Concept must have a manager"],
     },
-  ],
-  type: {
-    type: String,
-    enum: ["HOTPOT", "BBQ", "JAPANESE", "STEAK HOUSE", "OTHER"],
-    required: [true, "Concept must be have type."],
+    totalProfit: {
+      type: Number,
+      require: [true, "Concept Restaurant must be have total profit."],
+    },
+    imageCover: {
+      type: String,
+      required: [true, "Restaurant must be have imageCover."],
+    },
+    timeSlot: [
+      {
+        startTime: String,
+        endTime: String,
+        available: Boolean,
+      },
+    ],
+    dishes: [
+      {
+        name: { type: String, required: true },
+        description: { type: String },
+        type: { type: String, required: true },
+        image: { type: String },
+        price: { type: Number, required: true },
+      },
+    ],
+    type: {
+      type: String,
+      enum: ["HOTPOT", "BBQ", "JAPANESE", "STEAK HOUSE", "OTHER"],
+      required: [true, "Concept must be have type."],
+    },
+    avgRatings: {
+      type: Number,
+      default: 4.5,
+    },
   },
-  avgRatings: {
-    type: Number,
-    default: 4.5,
-  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+conceptSchema.virtual("restaurants", {
+  ref: "Restaurant",
+  localField: "_id",
+  foreignField: "concept",
 });
 
 const ConceptRestaurantModel = mongoose.model("Concept", conceptSchema);
