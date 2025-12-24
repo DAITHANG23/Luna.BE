b57121e82c5c4a0d44634c61b13dff709fc2dede
Dom Nguyen <nguyendaithang23061997@gmail.com>
2025-12-24 17:49:49 +0700

    feat(update): update logic and refactor code

--- controllers/bookingController.ts
+++ controllers/bookingController.ts
@@ -7,7 +7,25 @@ import {
   updateOne,
 } from "./handlerFactory";
 
-export const getAllBookings = getAll(BookingModel, { path: "restaurant" });
+export const getAllBookings = getAll(BookingModel, { 
+  filterBuilder(req) {
+    const user = req.user;
+
+    if(user?.role === 'customer') {
+      return {customer: user._id}
+    }
+
+    if(req.body?.restaurantId) {
+      return {restaurant: req.body.restaurantId}
+    }
+
+    return {}
+  },
+
+  preQuery(query) {
+    return query.sort("-createdAt").populate('restaurant')
+  }
+ });
 export const getBooking = getOne(BookingModel, { path: "restaurant" });
 export const createBooking = createOne(BookingModel);
 export const updateBooking = updateOne(BookingModel);
